96309ff99ebbea42ad8447ce7de38b25808963f1
Himanshu Vaghela <HimanshuVaghela2020@gmail.com>
2022-02-16 22:40:45 +0530

    Feat: User Modal

--- src/modules/Users/Users.vue
+++ src/modules/Users/Users.vue
@@ -3,40 +3,103 @@
     <b-card-header>
       <div class="d-flex justify-content-between align-items-center">
         <h5>Users</h5>
-        <b-button class="mr-1" variant="info">
+        <b-button class="mr-1" variant="info" @click="onCreateUserClick">
           Create User
         </b-button>
       </div>
     </b-card-header>
     <b-card-body class="users-card-body">
-
+      <no-content :show="!users.length" />
+      <b-table
+        v-if="users.length"
+        class="mr-3 ml-3 mb-0 pb-0"
+        responsive="sm"
+        :fields="fields"
+        :items="users"
+        striped
+      >
+        <template v-slot:cell(actions)="{ item }">
+          <b-button
+            variant="success"
+            class="mr-1"
+            size="sm"
+            @click="onEditUserClick(item)"
+          >
+            Edit
+          </b-button>
+          <b-button
+            variant="danger"
+            class="mr-1"
+            size="sm"
+            @click="onDeleteUserClick(item)"
+          >
+            Delete
+          </b-button>
+        </template>
+      </b-table>
+      <table-pagination
+        v-if="users.length"
+        ref="pagination"
+        :total-rows="users.length"
+        @on-pagination-change="onPaginationChange"
+      >
+      </table-pagination>
     </b-card-body>
+    <user-modal />
   </b-card>
 </template>
 
 <script>
-  export default {
-    name: "Users",
-  }
+import UserModal from "./modals/UserModal";
+import { createNamespacedHelpers } from "vuex";
+import TablePagination from "../../core/components/table-pagination/TablePagination";
+import NoContent from "../../core/components/no-content/NoContent";
+
+const { mapActions } = createNamespacedHelpers("users");
+export default {
+  name: "Users",
+  components: { NoContent, TablePagination, UserModal },
+  data() {
+    return {
+      users: [],
+      fields: [
+        { key: "name", label: "Name" },
+        { key: "surname", label: "Surname" },
+        { key: "email", label: "Email" },
+        { key: "phone", label: "Phone Number" },
+        { key: "actions", label: "Action" },
+      ],
+    };
+  },
+  methods: {
+    ...mapActions(["showUserModal"]),
+    onCreateUserClick() {
+      this.showUserModal();
+    },
+    onEditUserClick() {},
+    onDeleteUserClick() {},
+    onPaginationChange() {},
+  },
+};
 </script>
 
 <style lang="scss" scoped>
-  .users {
-    height: 100%;
-    width: 100%;
-    margin: 2rem;
-  }
+.users {
+  height: 100%;
+  width: 100%;
+  //margin: 2rem;
+}
 
-  .users-card-body {
-    background: #E8E8E8;
-    padding: 0;
-    overflow-x: hidden;
-    overflow-y: auto;
-  }
+.users-card-body {
+  background: white;
+  padding: 0;
+  overflow-x: hidden;
+  overflow-y: auto;
+}
 
-  .page-content {
-    margin-left: 15%;
-    margin-right: 15%;
-    position: relative;
-  }
+.page-content {
+  margin-left: 15%;
+  margin-right: 15%;
+  position: relative;
+}
 </style>
---
+++ src/modules/Users/models/UserModel.js
@@ -0,0 +1,39 @@
+import BaseModel from "../../../core/components/input-widget/BaseModel";
+
+export default class UserModel extends BaseModel {
+  uuid = null;
+  name = null;
+  surname = null;
+  email = null;
+  password = null;
+  username = null;
+  phone = null;
+  phone_country_code = null;
+  image = null;
+
+  rules = {
+    name: "required",
+    surname: "required",
+    email: [{ rule: "required" }, { rule: "email" }],
+    password: "required",
+    username: "required",
+    phone: "required",
+    phone_country_code: "required",
+  };
+
+  attributeLabels = {
+    name: "Name",
+    surname: "Surname",
+    email: "Email Address",
+    password: "Password",
+    username: "Username",
+    phone: "Phone Number",
+    phone_country_code: "Phone Country Code",
+    image: "Image",
+  };
+
+  constructor(data) {
+    super();
+    Object.assign(this, { ...data });
+  }
+}
